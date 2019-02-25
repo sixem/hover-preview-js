edbe206474d246e1c62129e9aefba278266c2887
sixem <bentkrisell@gmail.com>
2019-02-25 02:56:34 +0100

    Structure

--- README.md
+++ README.md
@@ -1,18 +1,57 @@
 # image.preview.js
 A simple jQuery plugin that adds hoverable image and video previews to links and other elements. 
 
-## Requirements
-### [jQuery](https://github.com/jquery/jquery)
+## Installation
+This script depends on [jQuery](https://github.com/jquery/jquery).
 
+#### Script
+Download and include the script in your web page.
+```
+curl -LO https://raw.github.com/sixem/image.preview.js/master/image.preview.js
+```
+##### Minified version.
+```
+curl -LO https://raw.github.com/sixem/image.preview.js/master/image.preview.min.js
+```
 ## Usage
-To use this plugin you need to add the `preview` class to any link or element you want to have display a preview. If you are using a simple `a` element then all you have to do is add the class and the plugin will fetch the URL automatically.
+The simplest way to use this plugin is to initialize it when the page is done loading.
+```
+$(document).ready(function()
+{
+	$(document).imagePreview({
+		elements : ['a.preview', 'div.preview']
+	});
+});
+```
+The `elements` parameters are the elements that you want to have an image or video preview. `elements : ['a']` will apply it to every link on the page while `elements : ['a.preview']` will apply it to all links with the `preview` class etc.
 
-If you want to display a preview using something other than a regular link then you can use the `data-preview` attribute instead.
+The plugin will use the image or video URL stored in the `href` attribute, if no `href` attribute is found it'll instead look for a `data-preview` attribute, this is what you want to use if you are using elements that aren't `a` elements.
+### Options
+There are a few options that can be passed to the script in order to customzie it a little bit.
+#### Example
+```
+$(document).ready(function()
+{
+	$(document).imagePreview({
+		elements : ['a.preview'],
+		hoverDelay : 250,
+		margin : 12,
+		css : {
+			'border' : '4px solid red'
+		},
+		extensions : {
+			images : ['jpg', 'jpeg', 'png'],
+			videos : ['webm']
+		}
+	});
+});
+```
+Below is a list of the available options and their default value.
 
-#### Examples
-
-`<a class="preview" href="https://eyy.co/files/9158.jpg">Image</a>`
-
-`<div class="preview" style="display:inline-block;" data-preview="https://eyy.co/files/9158.jpg">Image</div>`
-
-The reason `inline-block` is being used for the `div` element is because the preview will be placed next to whatever element triggered it so a `div` element that is covering the entire page will not work with this plugin.
+Key | Default | Description
+----|---------|------------
+`elements` | | The elements that should have a hoverable preview.
+`hoverDelay` | 150 | Delay in milliseconds before the preview is shown.
+`css` | | Custom CSS rules that will be applied to the image or video.
+`margin` | 4 | Margin between the preview and the window borders.
+`extensions` | All | What extensions should have a hoverable preview (See above for an example).
--- image.preview.js
+++ image.preview.js
@@ -1,109 +1,169 @@
 /* [https://github.com/sixem/image.preview.js] */
 
-ipjs = {
-    hoverDelay : 150,
-    timer : undefined,
-    lastSrc : undefined,
-    lastExt : undefined,
-    lastTrigger : undefined,
-    current : undefined,
-    currentHeight : 0,
-    currentWidth : 0,
-    visible : false,
-    extensions : {
-        images : ['jpg','jpeg','gif','png','ico','svg','bmp'],
-        videos : ['mp4','webm']
-    },
-    css : {
-        'max-height' : 'calc(100% - 8px)',
-        'visibility' : 'hidden',
-        '-webkit-box-shadow' : '0px 0px 3px 0px rgba(0,0,0,0.35)',
-        '-moz-box-shadow' : '0px 0px 3px 0px rgba(0,0,0,0.35)',
-        'box-shadow' : '0px 0px 3px 0px rgba(0,0,0,0.35)',
-        'position' : 'fixed',
-        'top' : 4,
-        'left' : 0,
-        'z-index' : 4
-    }
-};
-
-function arrayContains(t, e)
+(function($)
 {
-    return $.inArray(t, e) > -1;
-}
+  $.fn.imagePreview = function(options)
+  {
+    var ipjs = this;
 
-function getElementPositions($t)
-{
-    $offsets = $t.offset();
+    ipjs.settings = $.extend({
+      hoverDelay : 150,
+      margin : 4,
+      elements : [],
+      extensions : {
+          images : ['jpg', 'jpeg', 'gif', 'png', 'ico', 'svg', 'bmp'],
+          videos : ['mp4', 'webm']
+      },
+    }, ipjs.settings, options);
 
-    return {
-        'top' : $offsets.top - $(window).scrollTop(),
-        'left' : $offsets.left - $(window).scrollLeft(),
-        'offset' : $offsets
+    ipjs.settings.styles = {
+          'max-height' : 'calc(100% - ' + (ipjs.settings.margin * 2) + 'px)',
+          'visibility' : 'hidden',
+          'position' : 'fixed',
+          'top' : ipjs.settings.margin,
+          'left' : 0,
+          'z-index' : 4
     };
-}
 
-function previewShow($e)
-{
-    if($e == false)
+    function arrayContains(t, e)
     {
-        $('body').find('#video-preview, #image-preview').remove();
+        return $.inArray(t, e) > -1;
+    }
+
+    function getElementPositions($t)
+    {
+        $offsets = $t.offset();
+
+        return {
+            'top' : $offsets.top - $(window).scrollTop(),
+            'left' : $offsets.left - $(window).scrollLeft(),
+            'offset' : $offsets
+        };
+    }
+
+    function previewAdjust()
+    {
+        if(ipjs.current == undefined)
+        {
+            return false;
+        }
+
+        $ww = $(window).width();
+
+        $offset = getElementPositions(ipjs.lastTrigger);
+
+        if(($ww / 2) > $ww - $offset['left'])
+        {
+            $pos = Math.floor($ww - $offset['left'] + 10);
+
+            ipjs.current.css({
+                'left' : '',
+                'right' : $pos + 'px',
+                'max-width' : (($ww - $pos) - ipjs.settings.margin) + 'px'
+            });
+        } else {
+            $pos = Math.floor($offset['left'] + ipjs.lastTrigger.width() + 10);
+
+            ipjs.current.css({
+                'left' : $pos + 'px',
+                'right' : '',
+                'max-width' : (($ww - $pos) - ipjs.settings.margin) + 'px'
+            });
+        }
+
+        if(ipjs.current.height() < ipjs.currentHeight)
+        {
+            ipjs.currentHeight = ipjs.current.height();
+        }
+
+        $x = ($offset['top'] - ipjs.currentHeight);
+
+        if($x > 10)
+        {
+            ipjs.current.css('margin-top', $x + 'px');
+        }
+
+        ipjs.current.css('visibility', 'visible');
+
+        ipjs.visible = true;
+    }
+
+    function previewShow(enable)
+    {
+      if(enable == false)
+      {
+        $previews = $('body').find('#video-preview, #image-preview');
+
+        if($previews.length > 0)
+        {
+          $previews.remove();
+        }
 
         ipjs.visible = false;
         ipjs.current = ipjs.lastSrc = undefined;
         ipjs.currentHeight = ipjs.currentWidth = 0;
 
         return false;
-    }
+      }
 
-    $data = ipjs.lastTrigger.data('preview');
+      $data = ipjs.lastTrigger.data('preview');
 
-    if($data !== false && $data !== undefined)
-    {
+      if($data !== false && $data !== undefined)
+      {
         ipjs.lastSrc = ipjs.lastTrigger.data('preview');
-    } else {
+      } else {
 
         $attr = ipjs.lastTrigger.attr('href');
 
         if(typeof $attr !== typeof undefined && $attr !== false)
         {
-            ipjs.lastSrc = $attr;
+          ipjs.lastSrc = $attr;
         }
-    }
+      }
 
-    if(ipjs.lastSrc == undefined)
-    {
+      if(ipjs.lastSrc == undefined)
+      {
         return false;
-    }
+      }
 
-    ipjs.lastExt = ipjs.lastSrc.split('.').pop().toLowerCase();
+      ipjs.lastExt = ipjs.lastSrc.split('.').pop().toLowerCase();
 
-    if(arrayContains(ipjs.lastExt, ipjs.extensions.images))
-    { 
+      if(arrayContains(ipjs.lastExt, ipjs.settings.extensions.images))
+      { 
         img = new Image(); img.orig = ipjs.lastSrc;
 
         $(img).on('load', function()
         {
-            if(!ipjs.visible)
+          if(!ipjs.visible)
+          {
+            if(this.orig == ipjs.lastSrc)
             {
-                if(this.orig == ipjs.lastSrc)
-                {
-                    $('body').prepend('<img id="image-preview" onload="getDimensions();" src="' + img.src + '">');
+              $('body').prepend('<img id="image-preview" src="' + img.src + '">');
 
-                    ipjs.current = $('body').find('#image-preview');
+              ipjs.current = $('body').find('#image-preview');
 
-                    ipjs.current.css(ipjs.css);
-                }
+              ipjs.current.on('load', function()
+              {
+                $(document).imagePreview.getDimensions();
+              });
 
-                return true;
+              if(ipjs.settings.css != undefined)
+              {
+                ipjs.current.css(ipjs.settings.styles).css(ipjs.settings.css);
+              } else {
+                ipjs.current.css(ipjs.settings.styles);
+              }
             }
+
+            return true;
+          }
         });
 
         img.src = ipjs.lastSrc;
-    }
+      }
 
-    if(arrayContains(ipjs.lastExt, ipjs.extensions.videos))
-    {
+      if(arrayContains(ipjs.lastExt, ipjs.settings.extensions.videos))
+      {
         $('body').prepend('<video id="video-preview" controls loop muted autoplay><source src=""></video>');
 
         $video = $('body').find('#video-preview').first();
@@ -112,141 +172,111 @@ function previewShow($e)
 
         $video.attr('data-orig', ipjs.lastSrc);
         $video.find('source').attr('src', ipjs.lastSrc);
-        $video.css(ipjs.css);
+
+        if(ipjs.settings.css != undefined)
+        {
+          $video.css(ipjs.settings.styles).css(ipjs.settings.css);
+        } else {
+          $video.css(ipjs.settings.styles);
+        }
 
         $video[0].load();
 
         waitForVideo($video);
+      }
     }
-}
 
-function waitForVideo($video)
-{
-    function checkLoad()
+    function waitForVideo($video)
     {
+      function checkLoad()
+      {
         if(ipjs.current == undefined || $video.data('orig') != ipjs.lastSrc)
         {
-            return false;
+          return false;
         }
 
         if($video.prop('readyState') > 3 && $video.width() > 0 && $video.height() > 0)
         {
-            $video.one('seeked').prop('currentTime', 0);
+          $video.one('seeked').prop('currentTime', 0);
 
-            ipjs.currentWidth = $video.width();
-            ipjs.currentHeight = $video.height();
+          ipjs.currentWidth = $video.width();
+          ipjs.currentHeight = $video.height();
 
-            previewAdjust();
+          previewAdjust();
 
-            return true;
+          return true;
         } else {
-            setTimeout(checkLoad, 100);
+          setTimeout(checkLoad, 100);
         }
-    }
+      }
 
-    checkLoad();
-}
+      checkLoad();
+    };
 
-function getDimensions()
-{
-    function checkLoad()
+    $.fn.imagePreview.getDimensions = function()
     {
+      function checkLoad()
+      {
         if(ipjs.current == undefined)
         {
-            return false;
+          return false;
         }
         
         if(ipjs.current.width() > 0 && ipjs.current.height() > 0)
         {
-            ipjs.currentWidth = ipjs.current.width();
-            ipjs.currentHeight = ipjs.current.height();
+          ipjs.currentWidth = ipjs.current.width();
+          ipjs.currentHeight = ipjs.current.height();
 
-            previewAdjust();
+          previewAdjust();
 
-            return true;
+          return true;
         } else {
-            setTimeout(checkLoad, 100);
+          setTimeout(checkLoad, 100);
         }
-    }
+      }
 
-    checkLoad();
-}
+      checkLoad();
+    };
 
-function previewAdjust()
-{
-    if(ipjs.current == undefined)
+    function bindHandlers()
     {
+      if(ipjs.settings.elements.length < 1)
+      {
         return false;
-    }
+      }
 
-    $ww = $(window).width();
+      $.each(ipjs.settings.elements, function(index, value)
+      {
+        $target = $(document).find(value);
 
-    $offset = getElementPositions(ipjs.lastTrigger);
-
-    if(($ww / 2) > $ww - $offset['left'])
-    {
-        $pos = Math.floor($ww - $offset['left'] + 10);
-
-        ipjs.current.css({
-            'left' : '',
-            'right' : $pos + 'px',
-            'max-width' : (($ww - $pos) - 4) + 'px'
-        });
-    } else {
-        $pos = Math.floor($offset['left'] + ipjs.lastTrigger.width() + 10);
-
-        ipjs.current.css({
-            'left' : $pos + 'px',
-            'right' : '',
-            'max-width' : (($ww - $pos) - 4) + 'px'
-        });
-    }
-
-    if(ipjs.current.height() < ipjs.currentHeight)
-    {
-        ipjs.currentHeight = ipjs.current.height();
-    }
-
-    $x = ($offset['top'] - ipjs.currentHeight);
-
-    if($x > 10)
-    {
-        ipjs.current.css('margin-top', $x + 'px');
-    }
-
-    ipjs.current.css('visibility', 'visible');
-
-    ipjs.visible = true;
-}
-
-$(document).ready(function()
-{
-
-$(document).on('mouseenter', '.preview', function(e)
-{
-  ipjs.lastTrigger = $(this);
-
-  ipjs.timer = setTimeout(
-    function()
-    {
-        if(!ipjs.visible)
+        if($target.length > 0)
         {
-            previewShow();
-        } else {
-            return true;
+          $(document).on('mouseenter', value, function(e)
+          {
+            ipjs.lastTrigger = $(this);
+
+            ipjs.timer = setTimeout(
+              function()
+              {
+                  if(!ipjs.visible)
+                  {
+                      previewShow();
+                  } else {
+                      return true;
+                  }
+                }, ipjs.settings.hoverDelay
+              );
+          });
+
+          $(document).on('mouseleave', value, function(e)
+          {
+              clearTimeout(ipjs.timer); previewShow(false);
+          });
         }
-      }, ipjs.hoverDelay
-    );
-});
+      });
+    }
 
-$(document).on('mouseleave', '.preview', function(e)
-{
-    clearTimeout(ipjs.timer); previewShow(false);
-});
+    bindHandlers();
+  };
 
-});
-
-$(window).resize(function()
-{
-    previewAdjust();
-});
+}(jQuery));
